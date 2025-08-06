9e80b6bc813c795d1ce5d16e6c353559fc3fcc78
Anandpatel09 <anandbca2021@gmail.com>
2025-08-06 23:52:12 +0530

    Add practice project of useState hook in react

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,40 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+ const [count,setCount]=useState(0)
+
+ const increase=()=>{
+  setCount(count+1);
+ }
+ 
+ const decrease=()=>{
+  if(count !=0){
+setCount(count-1);
+  }
+  
+ }
+
 
   return (
     <>
-      
+
+
+      <div className='sec1'> 
+        <h1 className='heading'> welcome to counter section</h1>
+       <div className="btn">
+        <div className='box1'>
+          <button onClick={increase}>click me to increase</button>
+       </div>
+       <div className="box2">
+        <button onClick={decrease}>click me to decrease</button>
+       </div>
+       </div>
+       <h1 className='count'>{count}</h1>
+       </div>
        
+
+
     </>
   )
 }
